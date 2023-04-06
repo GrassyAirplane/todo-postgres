c2e5bedcc69691fd575cb2d34dfc70411e4be457
GrassyAirplane <euanlimcs@gmail.com>
2023-04-06 12:04:12 +0800

    Creating Database

--- server/crud.js
+++ server/crud.js
@@ -1,6 +1,38 @@
-const app = require("./index")
+const index = require("./index")
+
+const app = index.app
+const pool = index.pool
 
 app.get("/", (req, res) => {
     res.send("Hello World")
 })
+
+// ROUTES //
+
+// create a todo
+
+app.post("/todos", async(req,res) => {
+    try {
+        console.log(req.body)
+
+        // Insertion code
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALYES($1)", 
+            [description]
+        )
+        
+    } catch (error) {
+        console.error(err.message)
+    }
+})
+
+// get all todos 
+
+// get a todo
+
+// update a todo
+
+// delete a todo
+
 
--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const pool = require("./db")
 
 //middleware
 app.use(cors())
+app.use(express.json())
 
 app.listen(5000, () => {
     console.log("server has started on port 5000")
 })
 
-// exporting app
-module.exports = app
+// Exporting app and pool
+module.exports = {
+    app: app,
+    pool: pool
+};
